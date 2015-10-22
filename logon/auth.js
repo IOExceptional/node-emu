85ab6e3ac55660ed79b087cff9601780ea478539
leewalkergm <leewalkergm@gmail.com>
2015-10-22 14:59:50 +0100

    Added request dealing code

--- logon/auth.js
+++ logon/auth.js
@@ -1,3 +1,7 @@
+var AUTH_LOGON_CHALLENGE = 0x00;
+var AUTH_LOGON_PROOF = 0x01;
+
+
 /* 
     AuthLogonChallenge - ClientData
     -------------------------------
@@ -16,33 +20,65 @@
     AccountName: 0,          char[]
 */
 
-var binary = require('binary');
+
+var dissolve = require('dissolve');
 var Put = require('put');
 
 var parseLogonChallenge = function(data, cb) {
-    var logonChallenge = binary()
-                            .word8u('Command')
-                            .word8u('Error')
-                            .word16lu('Size')
-                            .word64lu('GameName')
-                            .word8u('Version1')
-                            .word8u('Version2')
-                            .word8u('Version3')
-                            .word16lu('Build')
-                            .word64lu('Platform')
-                            .word64lu('OS')
-                            .word64lu('Country')
-                            .word32lu('WorldRegion_bias')
-                            .word32lu('IP')
-                            .word8u('AccountName_Length')
-                            .tap(function (vars) {
-                                console.log(this);
+    var logonChallenge = dissolve()
+                            .uint8('Command')
+                            .uint8('Error')
+                            .uint16be('Size')
+                            .tap(function () {
+                                this.string("GameName", 4)
+                                .uint8('Version0')
+                                .uint8('Version1')
+                                .uint8('Version2')
+                                .uint16le('Build')
+                                .uint8('Platform0')
+                                .uint8('Platform1')
+                                .uint8('Platform2')
+                                .uint8('Platform3')
+                                .uint8('OS0')
+                                .uint8('OS1')
+                                .uint8('OS2')
+                                .uint8('OS3')
+                                .tap(function () {
+                                    this.string("Country", 4)
+                                    .uint32be('WorldRegion_bias')
+                                    .uint32be('IP')
+                                    .uint8('AccountName_Length')
+                                    .tap(function () {
+                                        this.string("AccountName", this.vars.AccountName_Length)
+                                        .tap(function () {
+                                            console.log(this.vars);
 
-                                console.log("Accountname length:", vars.AccountName_Length);
-
-                                cb(vars);
+                                            cb();
+                                        })
+                                    });
+                                });
                             });
-}
+
+    logonChallenge.write(data);
+};
+
+parseLogonChallenge.prototype.parseString = function (name, length) {
+  var len = [name, "len"].join("_");
+
+  return this.uint16be(len).tap(function() {
+    this.buffer(name, this.vars[len] * length).tap(function() {
+      delete this.vars[len];
+
+      for (var i=0;i<this.vars[name].length/length;++i) {
+        var t = this.vars[name][i*length];
+        this.vars[name][i*length] = this.vars[name][i*length+1];
+        this.vars[name][i*length+1] = t;
+      }
+
+      this.vars[name] = this.vars[name].toString("ucs2");
+    });
+  });
+};
 
 var createLogonChallengeResponse = function () {
     return Put()
@@ -55,12 +91,24 @@ var createLogonChallengeResponse = function () {
 
 module.exports = function (client) {
     client.on('data', function(data) {
-        parseLogonChallenge(data, function (logonChallenge) {
+        var request = dissolve()
+            .uint8('Command')
+            .tap(function () {
+                var cmd = this.vars.Command;
+                if(cmd == AUTH_LOGON_CHALLENGE) {
+                    parseLogonChallenge(data, function (logonChallenge) {
 
-            var response = createLogonChallengeResponse();
+                        var response = createLogonChallengeResponse();
 
-            client.write(response);
-            client.pipe(client);
-        });
+                        client.write(response);
+                        client.pipe(client);
+                    });
+                }
+                else if(cmd == AUTH_LOGON_PROOF) {
+
+                }
+            });
+
+        request.write(data);
     });
 };
